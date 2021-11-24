79e37ab82c60baa9cee04a8232172427d6a114f7
S̷̗̗̑y̷͇̖̎n̴̘̒̈́t̷̼̭̚ȁ̷̘̬͆x̶̬̳̆͛V̸̢͈̊̒ơ̸̠̈́î̸̗͍͘d̷̜̈́͜ <71892765+xploitspeeds@users.noreply.github.com>
2021-11-23 20:03:11 -0500

    Update romlist.js

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,7 +1,7 @@
 var ROMLIST = [
     /*
-    {url:"roms/rom1.z64",title:"Game 1"},
-    {url:"roms/rom2.v64",title:"Game 2"},
-    {url:"roms/rom3.v64",title:"Game 3"},
+    {url:"roms/animalcrossing64.z64",title:"Animal Crossing"},
+    {url:"roms/mario64.z64",title:"Super Mario 64"},
+    {url:"roms/smash.z64",title:"Smash Bros. 64"},
     */
 ];
